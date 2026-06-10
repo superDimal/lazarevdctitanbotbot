7a51a85c88c4aba8736ae8a855de099c231a121d
Dima l <159548591+superDimal@users.noreply.github.com>
2026-06-09 21:18:21 -0400

    Update bot name from 'Titan Bot' to 'lazarevdc Bot'

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        // Text users will see (example: "Playing /help | Titan Bot").
+        // Text users will see (example: "Playing /help | lazarevdc Bot").
         name: "WORKING",
         // Activity type number (0 = Playing).
         type: 0, 
@@ -136,7 +136,7 @@ export const botConfig = {
     },
     footer: {
       // Default footer text used in bot embeds.
-      text: "Titan Bot",
+      text: "lazarevdc Bot",
       // Footer icon URL (null = no icon).
       icon: null,
     },
